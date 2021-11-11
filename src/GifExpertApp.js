2bb55726fc6399a9748f842fe0fd309867a740af
Anderson Lopez Melendres <anderslop10@outlook.com>
2021-11-10 23:20:35 -0500

    firts commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One puch man']);
+
+    // const handleAdd = () => {
+    //     setCategories([...categories,'HunterXHunter']);
+    // }; 
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr></hr>
+            <AddCategory setCategories={ setCategories }></AddCategory>
+
+            <ol>
+                {
+                    categories.map(category => (
+                        <GifGrid 
+                            key={ category }
+                            category= { category }
+                        />)
+                    )
+                }
+            </ol>
+        </>
+    );
+};
+
+
+export default GifExpertApp;
